d767af80e38adee877e88a46a20b67e0628012bb
AngelicaBento <angelicaibento@gmail.com>
2023-09-15 16:52:35 -0300

    Exercicios até o 30

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+
+//Lista dos exercícios
 const { somar} = require("./Exercicios/um.js");
 const { valor_receber } = require("./Exercicios/Dois.js");
 const { peso } = require("./Exercicios/tres.js");
@@ -22,6 +24,26 @@ const { jurosCapital } = require("./Exercicios/dezenove.js");
 const { valorTotalIPI } = require("./Exercicios/vinte.js");
 //const { investigacaoCriminal } = require("./Exercicios/vinteUm.js");
 const { velocidadeMulta } = require("./Exercicios/vintedois.js");
+const { escreverBatata } = require("./Exercicios/vintetres.js");
+const { tabuadaRepet } = require("./Exercicios/vintequatro.js");
+const { mediaAltura } = require("./Exercicios/vintecinco.js");
+const { mediaPesos } = require("./Exercicios/vinteseis.js");
+const { pesoElevador } = require("./Exercicios/vintesete.js");
+const { numerosNegativos } = require("./Exercicios/vinteoito.js");
+const { numerosDiversos } = require("./Exercicios/vintenove.js");
+const { numsAleatoriosEspecificos } = require("./Exercicios/trinta.js");
+
+//Lista dos desafios
+const { idadeMesesDias } = require("./Desafios/desafiozero.js");
+const { trocaValores } = require("./Desafios/desafioum.js");
+const { maior_num } = require("./Desafios/desafiodois.js");
+const { num_menor } = require("./Desafios/desafiotres.js");
+const { anoBissexto } = require("./Desafios/desafioquatro.js");
+const { NumPosParMult } = require("./Desafios/desafiocinco.js");
+const { valoresFibonacci } = require("./Desafios/desafioseis.js");
+const { valoresFibDez } = require("./Desafios/desafiosete.js");
+const { detalhesNotas } = require("./Desafios/desafiooito.js");
+
 
 
 
@@ -141,7 +163,7 @@ app.post('/api/exquinze', (req, res) => {
 })
 
 app.post('/api/exdezesseis', (req, res) => {
-  const result = impostoDeRenda(req.body.cpf, req.body.numDependentes, req.body.renda_mensal)
+  const result = impostoDeRenda(req.body.CPF, req.body.numDependentes, req.body.rendaMensal)
 
   res.status(200).json({message: result})
 })
@@ -152,29 +174,120 @@ app.post('/api/exdezessete', (req, res) => {
   res.status(200).json({message: `Média: ${result}`})
 })
 
-app.post('/api/exercicio18', (req, res) => {
+app.post('/api/exdezoito', (req, res) => {
   const resultado = custoCarro(req.body.custoCarro);
-  res.status(200).json({message: `O custo do carro é: ${resultado}`});
+  res.status(200).json({message: `O custo do carro para o consumidor final é: ${resultado}`});
 });
 
-app.post('/api/exercicio19', (req, res) => {
+app.post('/api/exdezenove', (req, res) => {
   const resultado = jurosCapital(req.body.capital, req.body.taxa, req.body.tempo);
   res.status(200).json({message: resultado});
 });
 
-app.post('/api/exercicio20', (req, res) => {
-  const resultado = valorTotalIPI(req.body.porcentagem, req.body.cp1, req.body.vp1, req.body.qntp1, req.body.cp2, req.body.vp2, req.body.qntp2);
+app.post('/api/exvinte', (req, res) => {
+  const resultado = valorTotalIPI(req.body.percentagem, req.body.cod_peca1, req.body.valor_peca1, req.body.qntde_peca1, req.body.cod_peca2, req.body.valor_peca2, req.body.qntde_peca2);
   res.status(200).json({message: `O valor total do IPI é: ${resultado}`});
 });
 
-/*app.post('/api/exercicio21', (req, res) => {
+/*app.post('/api/exvinteum', (req, res) => {
   const resultado = investigacaoCriminal(req.body.respostas);
   res.status(200).json({message: resultado});
 })*/
 
-app.post('/api/exercicio22', (req, res) => {
+app.post('/api/exvintedois', (req, res) => {
   const resultado = velocidadeMulta(req.body.velPermitida, req.body.velPraticada);
   res.status(200).json({message: resultado});
+});
+
+app.post('/api/exvintetres', (req, res) => {
+  const resultado = escreverBatata(req.body.num);
+  res.status(200).json({message: resultado});
+});
+
+app.post('/api/exvintequatro', (req, res) => {
+  const resultado = tabuadaRepet(req.body.num);
+  res.status(200).json({message: resultado});
+});
+
+app.post('/api/exvintecinco', (req, res) => {
+  const resultado = mediaAltura(req.body.qntdePessoas, req.body.alturas);
+  res.status(200).json({message: `A média das alturas é: ${resultado}m`});
+});
+
+app.post('/api/exvinteseis', (req, res) => {
+  const resultado = mediaPesos(req.body.pesos);
+  res.status(200).json({message: `A média dos pesos é: ${resultado}kg`});
+});
+
+app.post('/api/exvintesete', (req, res) => {
+  const resultado = pesoElevador(req.body.pesos);
+  res.status(200).json({message: resultado});
+});
+
+app.post('/api/exvinteoito', (req, res) => {
+  const numeros = req.body.numeros;
+  const resultado = numerosNegativos(numeros);
+  res.status(200).json({numbers: numeros, message: resultado});
+});
+
+app.post('/api/exvintenove', (_, res) => {
+  res.status(200).json({message: `Os números gerados são: ${numerosDiversos()}`});
+});
+
+app.post('/api/extrinta', (_, res) => {
+  res.status(200).json({message: `Os números gerados são: ${numsAleatoriosEspecificos()}`});
+});
+
+//DESAFIOS!
+
+app.post('/api/desafios/desafiozero', (req, res) => {
+  const resultado = idadeMesesDias(req.body.idade);
+  res.status(200).json({message: `${resultado}`});
+});
+
+app.post('/api/desafios/desafioum', (req, res) => {
+  const resultado = trocaValores(req.body.numeros);
+  res.status(200).json({message: `${resultado}`});
+});
+
+app.post('/api/desafios/desafiodois', (req, res) => {
+  const resultado = maior_num(req.body.numeros);
+  res.status(200).json({message: `${resultado}`});
+});
+
+app.post('/api/desafios/desafiotres', (req, res) => {
+  const resultado = num_menor(req.body.numeros);
+  res.status(200).json({message: `${resultado}`});
+});
+
+app.post('/api/desafios/desafioquatro', (req, res) => {
+  const resultado = anoBissexto(req.body.ano);
+  res.status(200).json({message: `${resultado}`});
+});
+
+app.post('/api/desafios/desafiocinco', (req, res) => {
+  const resultado = NumPosParMult(req.body.numeros);
+  res.status(200).json({message: `${resultado}`});
+});
+
+app.post('/api/desafios/desafioseis', (req, res) => {
+  const resultado = valoresFibonacci(req.body.qntdeTermos);
+  res.status(200).json({message: `OS NÚMEROS GERADOS FORAM: ${resultado}`});
+});
+
+app.post('/api/desafios/desafiosete', (_, res) => {
+  res.status(200).json({message: `${valoresFibDez()}`});
+});
+
+app.post('/api/desafios/desafiooito', (req, res) => {
+  const resultado = detalhesNotas(req.body.notas);
+  res.status(200).json({
+    maiorNota: resultado.maiorNota,
+    menorNota: resultado.menorNota,
+    mediaNotas: resultado.media,
+    maioresNotas: resultado.maioresNotas,
+    menoresNotas: resultado.menoresNotas
+  });
 });
 
 app.listen(port, () => {
